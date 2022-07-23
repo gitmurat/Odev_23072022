0beaaba316e49a2df8fefe8ba77f10b1f3aeb647
gitmurat <murat.kurt@windowslive.com>
2022-07-24 00:07:58 +0300

    Add project files.

---
+++ Class_List/Kisiler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_List
+{
+    internal class Kisiler
+    {
+        private int _TC { get; set; }
+        public int TC
+        {
+            get
+            {
+                return _TC;
+            }
+        }
+
+        private string _Isim { get; set; }
+        public string Isim
+        {
+            get
+            { return _Isim; }
+        }
+
+        private string _Soyisim { get; set; }
+        public string Soyisim
+        {
+            get
+            { return _Soyisim; }
+        }
+
+        public Kisiler(int tc, string isim, string soyisim)
+        {
+            _TC = tc;
+            _Isim = isim;
+            _Soyisim = soyisim;
+        }
+
+        public static Kisiler YeniKisi()
+        {
+            Console.WriteLine("Kisinin TC'sini giriniz: ");
+            string tc = Console.ReadLine();
+            int TC = Convert.ToInt32(tc);
+            Console.WriteLine("Kisinin İsmini giriniz: ");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Kisinin Soyismini giriniz: ");
+            string soyisim = Console.ReadLine();
+
+
+            Kisiler kisi = new Kisiler(TC, isim, soyisim);
+            return kisi;
+        }
+
+        public override string ToString()
+        {
+            return Isim + " " + Soyisim;
+        }
+    }
+}
---
+++ Class_List/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_List
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool KisiEkle = true;
+            Dictionary<int, Kisiler> sinif = new Dictionary<int, Kisiler>();
+            int count = 0;
+
+            while (KisiEkle)
+            {
+                Console.WriteLine("Sınıfa yeni bir kişi eklemek istiyor musunuz? Evet için 'E', Hayır için 'H' yazınız.");
+                string ekle = Console.ReadLine();
+
+                if (ekle == "E")
+                {
+                    Kisiler kisi1 = Kisiler.YeniKisi();
+                    sinif.Add(count, kisi1);
+                    count++;
+                    
+                }
+
+                else if (ekle == "H")
+                {
+                    Console.WriteLine("Kisi ekleme tamamlandı. Program kapatılıyor");
+                    KisiEkle = false;
+                }
+
+                else
+                {
+                    Console.WriteLine("Yanlış giriş yaptınız.");
+                    
+                }
+                
+                foreach (var key in sinif.Keys)
+                Console.WriteLine(key + " - "+ sinif[key]);
+            }
+        }
+    }
+}
